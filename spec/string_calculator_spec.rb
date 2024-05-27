d7faaa9cd17363155569386de97be522ab94526e
Iti Modi <itimodi@gmail.com>
2024-05-27 11:52:52 +0300

    Add Ruby Gem & Empty String Implementation

---
+++ spec/string_calculator_spec.rb
@@ -0,0 +1,14 @@
+# spec/string_calculator_spec.rb
+
+require 'rspec'
+require_relative '../lib/string_calculator'
+
+RSpec.describe StringCalculator do
+  let(:calculator) { StringCalculator.new }
+
+  describe '#add' do
+    it "returns 0 for an empty string" do
+      expect(calculator.add("")).to eq(0)
+    end
+  end
+end
